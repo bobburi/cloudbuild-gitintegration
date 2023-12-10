ce71328d2e8b6843079534dff61a396e09494534
bobburi <bobburirajesh06@gmail.com>
2023-12-10 10:38:44 +0530

    added

--- main.tf
+++ main.tf
@@ -1,17 +1,24 @@
 provider "google" {
 
-project = "ringed-trail-406913"
-
-  
+project = var.project_id
+ 
 }
 
 resource "google_storage_bucket" "gcs-bucket" {
-  name          = "prd-img-gcs-001-1234"
-  location      = "US"
-  force_destroy = true
+  name          = var.storagebucket_name
+  location      = var.location
+  force_destroy = var.force_destroy
   storage_class = "STANDARD"
 
   uniform_bucket_level_access = true
 
   
+}
+
+
+resource "google_compute_network" "vpc_network" {
+  project                 = var.project_id
+  name                    = "vpc-network1111"
+  auto_create_subnetworks = true
+  mtu                     = 1460
 }
